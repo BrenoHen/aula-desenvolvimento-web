f2b19a18ee549b3a067100e404406e092a443c99
BrenoHen <breno.devs@gmail.com>
2024-05-27 10:36:47 -0300

    funcoes callback 02

---
+++ Seção 6 Javascript Função/100.FuncoesCallback02.js
@@ -0,0 +1,14 @@
+const grades = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0];
+
+// not using callback
+const lowGrades1 = [];
+for (let i in grades) {
+    if(grades[i] < 7){
+        lowGrades1.push(grades[i]);
+    }
+}
+console.log(lowGrades1);
+
+// use callback
+const lowGrades2 = grades.filter((grade) => grade < 7);
+console.log(lowGrades2);
